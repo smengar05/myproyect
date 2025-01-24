194368df04306284030f1b29cef5a2de689b9782
smengar05 <smengar05@gmail.com>
2025-01-24 13:25:03 +0100

    version 1.01.001

---
+++ boards/views.py
@@ -0,0 +1,194 @@
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from boards.forms import NewTopicForm, PostForm
+from django.db.models import Count
+from boards.models import Board, Post, Topic
+from django.contrib.auth.decorators import login_required
+from django.views.generic import UpdateView, ListView
+from django.utils import timezone
+from django.utils.decorators import method_decorator
+
+# Create your views here.
+"""
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+"""
+# con GCBV
+class BoardListView(ListView):
+    model = Board
+    context_object_name = 'boards'
+    template_name = 'home.html'
+
+"""
+# con FBV
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    queryset = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(queryset, 20)
+
+    try:
+        topics = paginator.page(page)
+    except PageNotAnInteger:
+        # fallback to the first page
+        topics = paginator.page(1)
+    except EmptyPage:
+        # probably the user tried to add a page number
+        # in the url, so we fallback to the last page
+        topics = paginator.page(paginator.num_pages)
+
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
+# o de esta forma
+#    try:
+#        board = Board.objects.get(pk=pk)
+#    except Board.DoesNotExist:
+#        raise Http404
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
+"""
+# con GCBV
+class TopicListView(ListView):
+    model = Topic
+    context_object_name = 'topics'
+    template_name = 'topics.html'
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        kwargs['board'] = self.board
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, pk=self.kwargs.get('pk'))
+        queryset = self.board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+        return queryset
+
+@login_required
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = request.user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=request.user
+            )
+            return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'topic_posts.html'
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)
+        if not self.request.session.get(session_key, False):
+            self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True
+
+        kwargs['topic'] = self.topic
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.topic = get_object_or_404(Topic, board__pk=self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
+        queryset = self.topic.posts.order_by('created_at')
+        return queryset
+
+""" 
+# con FVB
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    topic.views += 1
+    topic.save()
+    return render(request, 'topic_posts.html', {'topic': topic})
+ """
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+
+            topic.last_updated = timezone.now()
+            topic.save()
+
+            topic_url = reverse('topic_posts', kwargs={'pk': pk, 'topic_pk': topic_pk})
+            topic_post_url = '{url}?page={page}#{id}'.format(
+                url=topic_url,
+                id=post.pk,
+                page=topic.get_page_count()
+            )
+
+            return redirect(topic_post_url)
+    else:
+        form = PostForm()
+    return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+
+"""
+FBV Vista Basada en Funciones
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'new_post.html', {'form': form})
+
+CBV Vista Basada en Clases
+class NewPostView(View):
+    def render(self, request):
+        return render(request, 'new_post.html', {'form': self.form})
+
+    def post(self, request):
+        self.form = PostForm(request.POST)
+        if self.form.is_valid():
+            self.form.save()
+            return redirect('post_list')
+        return self.render(request)
+
+    def get(self, request):
+        self.form = PostForm()
+        return self.render(request)
+
+GCBV Vista Genericas Basadas en Clases
+class NewPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('post_list')
+    template_name = 'new_post.html' 
+"""
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
---
+++ readme.md
@@ -0,0 +1,17 @@
+Programa realizado siguiendo el tutorial de
+https://simpleisbetterthancomplex.com/series/beginners-guide/1.11/
+
+tips
+instalar python
+crear entorno 
+python -m venv venv
+activar entorno
+instalar requerimientos
+pip install -r requeriments.txt
+#para crear los modelos en la db
+python manage.py makemigrations
+python manage.py migrate
+Crear superusuario
+python manage.py createsuperuser
+ejecutar servidor
+python manage.py runserver
